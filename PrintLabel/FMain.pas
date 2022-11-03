341b1b97b29dc038dfc238cc6dc32130d2113086
shishkinzel <sishkin_angtel@rambler.ru>
2022-11-03 17:00:03 +0300

    Новый проект

---
+++ PrintLabel/FMain.pas
@@ -0,0 +1,25 @@
+unit FMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.pngimage;
+
+type
+  TfrmMain = class(TForm)
+    fonMain: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ PrintLabel/PrintLabel.dpr
@@ -0,0 +1,14 @@
+program PrintLabel;
+
+uses
+  Vcl.Forms,
+  FMain in 'FMain.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
