9f4df27a0a7f9bcacee103427d529717788d05cb
shishkinzel <shishkin_angtel@rambler.ru>
2023-12-05 21:50:30 +0300

    New project

---
+++ NewPrintLabel/PMain.dpr
@@ -0,0 +1,14 @@
+program PMain;
+
+uses
+  Vcl.Forms,
+  FMain in 'FMain.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
